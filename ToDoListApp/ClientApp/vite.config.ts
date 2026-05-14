5880564a60d99f32baf7b6ae4c66531cfefd3983
Ярослав Кучер <kucheryaroslav2@gmail.com>
2026-05-15 01:33:58 +0300

    Database fix

--- ToDoListApp/ClientApp/vite.config.ts
+++ ToDoListApp/ClientApp/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5127",
+        target: "http://localhost:8080",
         changeOrigin: true
       }
     }
